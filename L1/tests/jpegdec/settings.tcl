e9dda3affacc17dfec670066db976935df9dc91d
s095339 <s095339@gmail.com>
2023-04-23 19:11:37 +0800

    a

---
+++ L1/tests/jpegdec/settings.tcl
@@ -0,0 +1,8 @@
+set XPART xcu50-fsvh2104-2-e
+set CSIM 0
+set CSYNTH 1
+set COSIM 1
+set VIVADO_SYN 0
+set VIVADO_IMPL 0
+set XF_PROJ_ROOT "/mnt/HLSNAS/04.HPOmGn/m111061549/LabC/Vitis_Libraries/codec"
+set CUR_DIR "/mnt/HLSNAS/04.HPOmGn/m111061549/LabC/Vitis_Libraries/codec/L1/tests/jpegdec"
